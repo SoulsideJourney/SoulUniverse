d26b5205aa9776950c88b98af326cd0be06dbfc7
Alpha Ori <souljrn@gmail.com>
2025-10-26 15:19:04 +0300

    Coordinates.IsNearby

--- SoulUniverse/Coordinates.cs
+++ SoulUniverse/Coordinates.cs
@@ -4,4 +4,10 @@ public struct Coordinates(int x, int y)
 {
     public int X = x;
     public int Y = y;
+
+    /// <summary> Разница координатах по горизонтали и вертикали 1 </summary>
+    public bool IsNearby(Coordinates other)
+    {
+        return X - other.X is 1 or -1 && Y - other.Y is 1 or -1;
+    }
 }
